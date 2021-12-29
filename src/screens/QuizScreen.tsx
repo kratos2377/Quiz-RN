b68c6978086856c20dd3ec5b8ec61f4a303c3385
SHOBHIT YADAV <shobityadav23@gmail.com>
2021-12-29 00:57:11 -0800

    Quiz Section Complete. Only SQLITE Functions remain

--- src/screens/QuizScreen.tsx
+++ src/screens/QuizScreen.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Appbar, Button, RadioButton } from "react-native-paper";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  Appbar,
+  Button,
+  Dialog,
+  Paragraph,
+  Portal,
+  Provider,
+  RadioButton,
+} from "react-native-paper";
 import { MainNavProps } from "../utils/MainParamList";
+import ConfettiCannon from "react-native-confetti-cannon";
 
 interface QuizScreenProps {}
 
 export const QuizScreen = ({ navigation, route }: MainNavProps<"Quiz">) => {
   const [questions, setQuestions] = useState([...route?.params?.question]);
-  console.log(questions);
+
   const [ind, setInd] = useState(0);
-  const [checked, setChecked] = React.useState("first");
+  const [visible, setVisible] = useState(false);
   const [score, setScore] = useState(0);
   const [answers, setAnswers] = useState([...questions[ind].answers]);
   const [answerSelected, setAnswerSelected] = useState("");
@@ -33,88 +42,112 @@ export const QuizScreen = ({ navigation, route }: MainNavProps<"Quiz">) => {
   };
 
   const exitQuiz = () => {
+    setVisible(false);
     navigation.pop();
   };
 
+  const submitQuiz = () => {
+    setVisible(true);
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <Appbar>
-        <Appbar.Content title={"Question " + (ind + 1).toString()} />
-        <Appbar.Content title={score} />
-      </Appbar>
-      <View style={{ marginVertical: 30, marginHorizontal: 20 }}>
-        <Text>{questionAsked}</Text>
-      </View>
+    <Provider>
+      <View style={{ width: "100%", height: "100%" }}>
+        <Portal>
+          <Dialog visible={visible}>
+            <Dialog.Content>
+              <ConfettiCannon count={200} origin={{ x: -10, y: 0 }} />
+              <Paragraph>Congrats...!!!</Paragraph>
+              <Text>You have completed the Quiz</Text>
+              <Paragraph>Your final Score is: {score} Points</Paragraph>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={exitQuiz}>Start a new Quiz</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
 
-      <View>
-        <RadioButton.Group
-          onValueChange={(answer) => {
-            setAnswerSelected(answer);
-            setPressed(true);
-          }}
-          value={answerSelected}
-        >
-          <RadioButton.Item
-            position="leading"
-            label={answers[0]}
-            value={answers[0]}
-          />
-          <RadioButton.Item
-            position="leading"
-            label={answers[1]}
-            value={answers[1]}
-          />
-          <RadioButton.Item
-            position="leading"
-            label={answers[2]}
-            value={answers[2]}
-          />
-          <RadioButton.Item
-            position="leading"
-            label={answers[3]}
-            value={answers[3]}
-          />
-        </RadioButton.Group>
-      </View>
+        <View style={styles.container}>
+          <Appbar>
+            <Appbar.Content title={"Question " + (ind + 1).toString()} />
+            <Appbar.Content title={score} />
+          </Appbar>
+          <View style={{ marginVertical: 30, marginHorizontal: 20 }}>
+            <Text>{questionAsked}</Text>
+          </View>
 
-      {ind != questions.length - 1 ? (
-        <Button
-          mode="outlined"
-          icon="arrow-right"
-          style={styles.nextButton}
-          onPress={nextQuestion}
-          disabled={!pressed}
-        >
-          Next
-        </Button>
-      ) : (
-        <Button
-          mode="outlined"
-          icon="arrow-right"
-          style={styles.nextButton}
-          disabled={!pressed}
-          onPress={() => {
-            console.log("Pressed Submit");
-          }}
-        >
-          Submit
-        </Button>
-      )}
-    </View>
+          <View>
+            <RadioButton.Group
+              onValueChange={(answer) => {
+                setAnswerSelected(answer);
+                setPressed(true);
+              }}
+              value={answerSelected}
+            >
+              <RadioButton.Item
+                position="leading"
+                label={answers[0]}
+                value={answers[0]}
+              />
+              <RadioButton.Item
+                position="leading"
+                label={answers[1]}
+                value={answers[1]}
+              />
+              <RadioButton.Item
+                position="leading"
+                label={answers[2]}
+                value={answers[2]}
+              />
+              <RadioButton.Item
+                position="leading"
+                label={answers[3]}
+                value={answers[3]}
+              />
+            </RadioButton.Group>
+          </View>
+
+          {ind != questions.length - 1 ? (
+            <Button
+              mode="outlined"
+              icon="arrow-right"
+              style={styles.nextButton}
+              onPress={nextQuestion}
+              disabled={!pressed}
+            >
+              Next
+            </Button>
+          ) : (
+            <Button
+              mode="outlined"
+              icon="arrow-right"
+              style={styles.nextButton}
+              disabled={!pressed}
+              onPress={() => {
+                setVisible(true);
+              }}
+            >
+              Submit
+            </Button>
+          )}
+        </View>
+      </View>
+    </Provider>
   );
 };
 
 const styles = StyleSheet.create({
-  bottom: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
   nextButton: {
     position: "absolute",
     margin: 16,
     right: 10,
     bottom: 10,
+  },
+
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    flexDirection: "column",
   },
 });
